c27de6de301170365f2ff57f5475ba2ec473a8c3
Andon <andonteoh.21@ichat.sp.edu.sg>
2023-10-31 11:10:30 +0800

    Finished unit testing code for bmi function in sub function Lab2

--- Test_bmi.py
+++ Test_bmi.py
@@ -1,8 +1,18 @@
-import unittest
+import Lab2.Lab2 as bmi
+#import the file then after the dot is the name of the .py file you wanna use for unit testing
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
 
-if __name__ == '__main__':
-    unittest.main()
+print("Test_Lab3")
+
+
+def test_Underweight():
+    assert(-1 == bmi.calculate_bmi(height = 1.5, weight = 30)) # use the name that represent Lab2 up there and then . name of function you wish to test in Lab2.py
+
+
+def test_Normalweight():
+    assert(0 == bmi.calculate_bmi(height = 1.7, weight = 60))
+
+def test_Overweight():
+     assert(1 == bmi.calculate_bmi(height = 1.7, weight = 100))
+
+
